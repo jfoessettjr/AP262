6031e5be450242d4555610bcd2fc93f7f098c114
jtfoess <jtfoess@pointpark.edu>
2023-08-07 06:59:51 -0400

    updated server.js, routes.js

--- API_ProductName/server.js
+++ API_ProductName/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const beerRoutes = require("./src/beers/routes");
+
 const app = express();
 const port = 6007;
 
@@ -7,5 +9,7 @@ app.use(express.json());
 app.get("/", (req, res)=> {
     res.send("hello world");
 })
+
+app.use("/api_productname/v1/beers", beerRoutes);
 
 app.listen(port, () => console.log('running active port'));
--- API_ProductName/src/beers/routes.js
+++ API_ProductName/src/beers/routes.js
@@ -1,9 +1,14 @@
 const { Router } = require("express");
+const controller = require('./controller');
 
 const router = Router();
 
+router.get('/', controller.getBeers);
+
+/*
 router.get('/', (req,res)=> {
     res.send("using api route");
 });
+*/
 
 module.exports = router;
