9df9b8587f8e1c103d62dbc8375114df50715d80
jtfoess <jtfoess@pointpark.edu>
2023-08-24 20:54:01 -0400

    finished getBeers.html

--- js/getBeers.js
+++ js/getBeers.js
@@ -1,3 +1,19 @@
 fetch("https://beersapc.onrender.com/api_productname/v1/beers").then((data)=>{
-    console.log(data);
+    //console.log(data);
+    return data.json();
+}).then((objectData)=>{
+    console.log(objectData[0].id);
+    let beerData="";
+    objectData.map((values)=>{
+        beerData+=`<tr>
+        <td>${values.beer}</td>
+        <td>${values.abv}</td>
+        <td>${values.style}</td>
+        <td>${values.brewery}</td>
+        <td>${values.beerId}/<td>
+      </tr>`;
+    });
+    document.getElementById("beer_body").innerHTML=beerData;
+}).catch((err)=>{
+    console.log(err);
 })
